fc40332a385b5ddb07acb68fd0470e0bf824864c
Khrononian <71100908+Khrononian@users.noreply.github.com>
2021-11-24 14:34:28 -0500

    Cleaned code
    Made forecasting into a imported function for shorter and more readable code

--- src/Functionality/api.js
+++ src/Functionality/api.js
@@ -1,4 +1,5 @@
 import weatherForecasting from './dom'
+import { postWeatherForecastData } from './dom';
 
 const input = document.querySelector('.search-input');
 const dayFind = document.querySelector('.days')
@@ -8,10 +9,7 @@ input.addEventListener('search', findCityWeather)
 function findCityWeather() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=imperial&appid=720db497deedc97541097097eaee6cfd`)
     .then(response => response.json())
-    .then(data => {
-        // ADD A WAY TO PREVENT searching a non existing city
-        console.log(data)
-
+    .then(data => {        
         async function getAllInfo() {
             try {
                 const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&exclude=minutely,alerts&appid=720db497deedc97541097097eaee6cfd`)
@@ -26,20 +24,15 @@ function findCityWeather() {
                 }
                 allData.daily.forEach((value, index) => {
                     const dayForecast = new Date(value.dt * 1000).toLocaleDateString('en', {weekday: 'long'})
-                    const forecast = 
-                        `<div class="day">
-                        <h5>${dayForecast}</h5>
-                        <img class="imgs" src="https://openweathermap.org/img/wn/${value.weather[0].icon}@2x.png">
-                        <p class="temp temps">${Math.round(value.temp.day)}°F</p>
-                        <p class="weather">${value.weather[0].main}</p>
-                        </div>`
+                    const weatherCasting = postWeatherForecastData(value, dayForecast)
 
-                    if (index > 0) dayFind.insertAdjacentHTML('beforeend', forecast)
+                    if (index > 0) dayFind.insertAdjacentHTML('beforeend', weatherCasting)
                 })
 
                 console.log(allData, allData.daily[0].dt, new Date(allData.daily[0].dt))
             } catch (error) {
-                console.log(error)
+                input.value = ''
+                return
             }
         }
         getAllInfo()
@@ -55,21 +48,15 @@ window.addEventListener('load', () => {
             try {
                 const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${loadedData.coord.lat}&lon=${loadedData.coord.lon}&units=imperial&exclude=minutely,alerts&appid=720db497deedc97541097097eaee6cfd`)
                 const weatherData = await response.json()
-
+            
                 weatherForecasting(weatherData)
 
                 console.log('Weather', weatherData)
                 weatherData.daily.forEach((value, index) => {
                     const dayForecast = new Date(value.dt * 1000).toLocaleDateString('en', {weekday: 'long',})
-                    const forecast = 
-                    `<div class="day">
-                    <h5>${dayForecast}</h5>
-                    <img class="imgs" src="https://openweathermap.org/img/wn/${value.weather[0].icon}@2x.png">
-                    <p class="temp temps">${Math.round(value.temp.day)}°F</p>
-                    <p class="weather">${value.weather[0].main}</p>
-                    </div>`
+                    const weatherCasting = postWeatherForecastData(value, dayForecast)
                        
-                    if (index > 0) dayFind.insertAdjacentHTML('beforeend', forecast)
+                    if (index > 0) dayFind.insertAdjacentHTML('beforeend', weatherCasting)
                 })
             } catch (error) {
                 console.log(error)
