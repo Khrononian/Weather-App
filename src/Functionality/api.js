0953645e781a40c29587d6f20c38a2e7611e6205
Khrononian <71100908+Khrononian@users.noreply.github.com>
2021-11-26 17:06:10 -0500

    Finishing touches

--- src/Functionality/api.js
+++ src/Functionality/api.js
@@ -1,26 +1,27 @@
-import weatherForecasting from './dom'
-import { postWeatherForecastData } from './dom';
+import weatherForecasting, { postWeatherForecastData } from './dom'
 
 const input = document.querySelector('.search-input');
-const dayFind = document.querySelector('.days')
+const dayFind = document.querySelector('.days');
+const tempContain = document.querySelector('.temp-contain');
 
-input.addEventListener('search', findCityWeather)
+input.addEventListener('search', findCityWeather);
 
 function findCityWeather() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=imperial&appid=720db497deedc97541097097eaee6cfd`)
     .then(response => response.json())
-    .then(data => {        
+    .then(data => {
         async function getAllInfo() {
             try {
-                const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&exclude=minutely,alerts&appid=720db497deedc97541097097eaee6cfd`)
+                let response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=imperial&exclude=hourly,minutely,alerts&appid=720db497deedc97541097097eaee6cfd`)
+                if (tempContain.dataset.temp == 'cels') response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=metric&exclude=hourly,minutely,alerts&appid=720db497deedc97541097097eaee6cfd`)
                 const allData = await response.json()
-
+                
                 document.querySelector('h3').innerText = input.value;
-                weatherForecasting(allData)
-                input.value = ''
+                weatherForecasting(allData);
+                input.value = '';
                 
                 while (dayFind.hasChildNodes()) {
-                    dayFind.removeChild(dayFind.firstChild)
+                    dayFind.removeChild(dayFind.firstChild);
                 }
                 allData.daily.forEach((value, index) => {
                     const dayForecast = new Date(value.dt * 1000).toLocaleDateString('en', {weekday: 'long'})
@@ -29,10 +30,17 @@ function findCityWeather() {
                     if (index > 0) dayFind.insertAdjacentHTML('beforeend', weatherCasting)
                 })
 
-                console.log(allData, allData.daily[0].dt, new Date(allData.daily[0].dt))
+                if (tempContain.dataset.temp == 'cels') {
+                    const allTemps = document.querySelectorAll('.temps')
+
+                    allTemps.forEach(v => {
+                        v.innerText = v.innerText.replace(/[^0-9]/g, '')
+                        v.innerText += '°C'
+                    })
+                }
             } catch (error) {
                 input.value = ''
-                return
+                throw error
             }
         }
         getAllInfo()
@@ -43,23 +51,21 @@ window.addEventListener('load', () => {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=720db497deedc97541097097eaee6cfd')
     .then(response => response.json())
     .then(loadedData => {
-        console.log(loadedData)
         async function loadCityWeather() {
             try {
-                const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${loadedData.coord.lat}&lon=${loadedData.coord.lon}&units=imperial&exclude=minutely,alerts&appid=720db497deedc97541097097eaee6cfd`)
+                const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${loadedData.coord.lat}&lon=${loadedData.coord.lon}&units=imperial&exclude=hourly,minutely,alerts&appid=720db497deedc97541097097eaee6cfd`)
                 const weatherData = await response.json()
             
                 weatherForecasting(weatherData)
 
-                console.log('Weather', weatherData)
                 weatherData.daily.forEach((value, index) => {
-                    const dayForecast = new Date(value.dt * 1000).toLocaleDateString('en', {weekday: 'long',})
+                    const dayForecast = new Date(value.dt * 1000).toLocaleDateString('en', {weekday: 'long'})
                     const weatherCasting = postWeatherForecastData(value, dayForecast)
                        
                     if (index > 0) dayFind.insertAdjacentHTML('beforeend', weatherCasting)
                 })
             } catch (error) {
-                console.log(error)
+                throw error
             }
         }
         loadCityWeather()
